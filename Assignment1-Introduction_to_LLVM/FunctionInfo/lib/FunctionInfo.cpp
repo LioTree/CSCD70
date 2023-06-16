a95c803add6c52e4c76497a649681219aefdad6b
LioTree <uc0110@163.com>
2023-06-16 22:52:33 +0800

    Assignment1: finish FunctionInfo, pass all tests

--- Assignment1-Introduction_to_LLVM/FunctionInfo/lib/FunctionInfo.cpp
+++ Assignment1-Introduction_to_LLVM/FunctionInfo/lib/FunctionInfo.cpp
@@ -12,7 +12,42 @@ public:
     outs() << "CSCD70 Function Information Pass"
            << "\n";
 
-    /// @todo(CSCD70) Please complete this method.
+    /// @done(CSCD70) Please complete this method.
+    for (Function &F : M) {
+      // 1. Name
+      StringRef functionName = F.getName();
+      outs() << "Function Name: " << functionName << "\n";
+
+      // 2. Number of Arguments
+      unsigned int numArgs = F.arg_size();
+      if (F.isVarArg())
+        outs() << "Number of Arguments: " << numArgs << "+*\n";
+      else
+        outs() << "Number of Arguments: " << numArgs << "\n";
+
+      // 3. Number of Direct Call Sites
+      unsigned int numDirectCalls = 0;
+      for (User *U : F.users()) {
+        if (Instruction *I = dyn_cast<Instruction>(U)) {
+            numDirectCalls++;
+        }
+      }
+      outs() << "Number of Calls: " << numDirectCalls << "\n";
+
+      // 4. Number of Basic Blocks
+      unsigned int numBasicBlocks = 0;
+      for (BasicBlock &BB : F) {
+        numBasicBlocks++;
+      }
+      outs() << "Number OF BBs: " << numBasicBlocks << "\n";
+
+      // 5. Number of Instructions
+      unsigned int numInstructions = 0;
+      for (BasicBlock &BB : F) {
+        numInstructions += BB.size();
+      }
+      outs() << "Number of Instructions: " << numInstructions << "\n";
+    }
 
     return PreservedAnalyses::all();
   }
